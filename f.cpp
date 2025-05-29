43ca928cbaf26b9af4d1608e9b94510077a8c6ab
Arkhip <108357450+vvmutee@users.noreply.github.com>
2025-05-29 23:15:33 +0200

    Update f.cpp

--- f.cpp
+++ f.cpp
@@ -1,128 +1,149 @@
 #include <iostream>
-#include <vector>
-#include <stdexcept>
+
 
 class BoolVector {
 private:
-    std::vector<uint8_t> data;
-    size_t bit_size = 0;
-
-    // Получить байт и маску по индексу
-    static std::pair<size_t, uint8_t> getByteIndexAndMask(size_t index) {
-        size_t byte_index = index / 8;
-        uint8_t mask = 1 << (index % 8);
-        return {byte_index, mask};
-    }
+    char* data;
+    size_t bytes;
+    size_t bits;
 
 public:
-    // Прокси-класс для доступа к отдельным битам
-    class Reference {
-        friend class BoolVector;
-        uint8_t& byte;
-        uint8_t mask;
+    // Конструктор по умолчанию
+    BoolVector() {
+        data = nullptr;
+        bytes = 0;
+        bits = 0;
+    }
 
-        Reference(uint8_t& b, uint8_t m) : byte(b), mask(m) {}
+    // Деструктор
+    ~BoolVector() {
+        delete[] data;
+    }
 
-    public:
-        operator bool() const {
-            return byte & mask;
-        }
+    // Установка конкретного бита в 0
+    void set_0(size_t index) {
+        size_t index_byte = index / 8;  // В каком байте находится нужный бит
+        size_t index_bit = index % 8;   // Позиция бита в байте (0-7)
+        data[index_byte] &= ~(1 << index_bit);
+        // сдвигаем 1 влево на кол-во битов, меняем ее на 0 и умножением меняем в исходном векторе
+    }
 
-        Reference& operator=(bool value) {
-            if (value)
-                byte |= mask;
-            else
-                byte &= ~mask;
-            return *this;
-        }
+    // Установка конкретного бита в 1
+    void set_1(size_t index) {
+        size_t index_byte = index / 8;
+        size_t index_bit = index % 8;
+        data[index_byte] |= (1 << index_bit);
+        // тоже, но устанавливаем 1
+    }
 
-        Reference& operator=(const Reference& other) {
-            return *this = static_cast<bool>(other);
-        }
-    };
+    // Получение значения бита по индексу
+    bool get(size_t index) {
+        size_t index_byte = index / 8;
+        size_t index_bit = index % 8;
+        return (data[index_byte] >> index_bit) & 1;
+    }
 
-    // Добавление в конец
-    void push_back(bool value) {
-        if (bit_size % 8 == 0) {
-            data.push_back(0);
-        }
-        auto [byte_index, mask] = getByteIndexAndMask(bit_size);
+    // Оператор доступа по индексу
+    bool operator[](size_t index) {
+        return get(index);
+    }
+
+    // Универсальный метод установки бита
+    void set_bit(size_t index, bool value) {
         if (value)
-            data[byte_index] |= mask;
-        ++bit_size;
+            set_1(index);
+        else
+            set_0(index);
     }
 
-    // Метод size
-    size_t size() const {
-        return bit_size;
+    // Добавление бита в конец вектора
+    void push_back(bool value) {
+        // Если память заполнена - увеличиваем
+        if (bits == bytes * 8) {
+            size_t new_bytes = bytes ? bytes * 2 : 1;  // Удваиваем или берём 1 байт
+            char* newdata = new char[new_bytes];      // Выделяем новую память
+
+            // Копируем старые данные
+            for (size_t i = 0; i < bytes; i++) {
+                newdata[i] = data[i];
+            }
+
+            delete[] data;    // Освобождаем старую память
+            data = newdata;   // Обновляем указатель
+            bytes = new_bytes; // Запоминаем новый размер
+        }
+        set_bit(bits, value);  // Устанавливаем бит
+        bits++;                // Увеличиваем счётчик битов
     }
 
-    // Получение значения по индексу
-    bool operator[](size_t index) const {
-        if (index >= bit_size) throw std::out_of_range("Index out of bounds");
-        auto [byte_index, mask] = getByteIndexAndMask(index);
-        return data[byte_index] & mask;
+    // Возвращает текущее количество битов
+    size_t size() {
+        return bits;
     }
 
-    // Изменение значения по индексу
-    Reference operator[](size_t index) {
-        if (index >= bit_size) throw std::out_of_range("Index out of bounds");
-        auto [byte_index, mask] = getByteIndexAndMask(index);
-        return Reference(data[byte_index], mask);
-    }
-
-    // Метод insert
+    // Вставка бита в произвольную позицию
     void insert(size_t index, bool value) {
-        if (index > bit_size) throw std::out_of_range("Index out of bounds");
-
-        push_back(false); // расширить размер
-        for (size_t i = bit_size - 1; i > index; --i) {
-            (*this)[i] = (*this)[i - 1];
+        // Сдвигаем все биты после index вправо
+        for (size_t i = bits - 1; i > index; i--) {
+            set_bit(i, get(i - 1));
         }
-        (*this)[index] = value;
+        set_bit(index, value);  // Вставляем новый бит
+        bits++;                // Увеличиваем счётчик
     }
 
-    // Метод erase
+    // Удаление бита из произвольной позиции
     void erase(size_t index) {
-        if (index >= bit_size) throw std::out_of_range("Index out of bounds");
-
-        for (size_t i = index; i < bit_size - 1; ++i) {
-            (*this)[i] = (*this)[i + 1];
+        // Сдвигаем все биты после index влево
+        for (size_t i = index; i < bits - 1; i++) {
+            set_bit(i, get(i + 1));
         }
-        --bit_size;
-        if (bit_size % 8 == 0)
-            data.pop_back();
+        bits--;  // Уменьшаем счётчик
     }
 
-    // Вывод содержимого вектора
-    void print() const {
-        for (size_t i = 0; i < bit_size; ++i)
-            std::cout << (*this)[i] << " ";
-        std::cout << '\n';
+    void print() {
+        std::cout << "Bits (" << bits << "): [ ";
+        for (size_t i = 0; i < bits; ++i) {
+            // Получаем значение бита
+            size_t byte_idx = i / 8;
+            size_t bit_idx = i % 8;
+            bool bit_value = (data[byte_idx] >> bit_idx) & 1;
+
+            // Выводим бит (1 или 0)
+            std::cout << bit_value << " ";
+
+            // Добавляем разделитель каждые 8 бит для удобства чтения
+            if ((i + 1) % 8 == 0 && (i + 1) != bits) {
+                std::cout << "| ";
+            }
+        }
+        std::cout << "]" << std::endl;
     }
 };
+
 int main() {
-    BoolVector bv;
-    bv.push_back(true);
-    bv.push_back(false);
-    bv.push_back(true);
-    bv.push_back(true);
-    std::cout << "Изначальный вектор: ";
-    bv.print(); // 1 0 1 1
+    std::setlocale(LC_ALL, "Russian");
+    BoolVector a;
 
-    std::cout << "Размер: " << bv.size() << "\n";
+    a.push_back(true);
+    for (int i = 0; i < 7; i++) a.push_back(0);
+    a.print();
 
-    std::cout << "bv[2] = " << bv[2] << "\n";
-    bv[1] = true;
-    std::cout << "После bv[1] = true: ";
-    bv.print(); // 1 1 1 1
+    std::cout << "Удаляем бит с индексом 1" << std::endl;
+    a.erase(1);  // Удаляем второй бит (индекс 1)
+    a.print();
 
-    bv.insert(2, false);
-    std::cout << "После insert(false, 2): ";
-    bv.print(); // 1 1 0 1 1
+    std::cout << "Ставим в начало 1" << std::endl;
+    a.insert(0, 1);
+    a.print();
 
-    bv.erase(3);
-    std::cout << "После erase(3): ";
-    bv.print(); // 1 1 0 1
+    std::cout << "Ставим 0 в начало" << std::endl;
+    a.set_0(0);
+    a.print();
+
+    a.push_back(1);
+    a.print();
+
+    for (int i = 0; i < 8; i++) a.push_back(1);
+    a.print();
+    return 0;
 }
-
